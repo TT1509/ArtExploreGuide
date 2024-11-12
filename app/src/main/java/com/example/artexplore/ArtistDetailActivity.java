ccfcab319fa2f6874e7ccf4f96ac7c7bb5b7bae8
TT1509 <s3978481@rmit.edu.vn>
2024-11-12 23:15:40 +0700

    Update Artist Detail Activity

--- app/src/main/java/com/example/artexplore/ArtistDetailActivity.java
+++ app/src/main/java/com/example/artexplore/ArtistDetailActivity.java
@@ -5,6 +5,8 @@ import android.util.Log;
 import android.widget.ImageView;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.artexplore.model.Artwork;
 import com.example.artexploreguide.R;
 import com.example.artexplore.model.Artist;
 
@@ -27,6 +29,7 @@ public class ArtistDetailActivity extends AppCompatActivity {
         TextView biographyTextView = findViewById(R.id.biographyTextView);
         TextView dateOfBirthTextView = findViewById(R.id.dateOfBirthTextView);
         TextView dateOfDeathTextView = findViewById(R.id.dateOfDeathTextView);
+        TextView artworksListTextView = findViewById(R.id.artworksListTextView);
 
         // Get artist name from Intent extras
         String artistName = getIntent().getStringExtra("ARTIST_NAME");
@@ -52,6 +55,13 @@ public class ArtistDetailActivity extends AppCompatActivity {
 
             // Load image from drawable resources
             artistImageView.setImageResource(artist.getImageResId());
+
+            // Display artworks titles
+            StringBuilder artworksListBuilder = new StringBuilder();
+            for (Artwork artwork : artist.getArtworks()) {
+                artworksListBuilder.append("• ").append(artwork.getTitle()).append("\n");
+            }
+            artworksListTextView.setText(artworksListBuilder.toString());
         } else {
             Log.e(TAG, "Artist details not found for name: " + artistName);
         }
