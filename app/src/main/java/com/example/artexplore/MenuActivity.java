2080c27f9b41b43befc72663d1587fea46176a69
TT1509 <s3978481@rmit.edu.vn>
2024-11-13 14:00:00 +0700

    Update

--- app/src/main/java/com/example/artexplore/MenuActivity.java
+++ app/src/main/java/com/example/artexplore/MenuActivity.java
@@ -5,11 +5,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 import com.example.artexploreguide.R;
 
@@ -22,6 +18,7 @@ public class MenuActivity extends AppCompatActivity {
 
         Button buttonArtStyles = findViewById(R.id.artStylesButton);
         Button buttonArtists = findViewById(R.id.artistsButton);
+        Button buttonArtworks = findViewById(R.id.artLocationButton);
 
         buttonArtStyles.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -34,6 +31,14 @@ public class MenuActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MenuActivity.this, ArtistsActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        buttonArtworks.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MenuActivity.this, AllArtworksActivity.class);
                 startActivity(intent);
             }
         });
